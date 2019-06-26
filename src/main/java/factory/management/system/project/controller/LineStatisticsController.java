e0e149669bdb3a7f21734f771f7706645b89972c
ngchunhoCR7 <cr7realmadrid@163.com>
2019-06-26 22:13:06 +0800

    添加导出表格功能

--- src/main/java/factory/management/system/project/controller/LineStatisticsController.java
+++ src/main/java/factory/management/system/project/controller/LineStatisticsController.java
@@ -1,9 +1,15 @@
 package factory.management.system.project.controller;
 
+import factory.management.system.project.entity.Line;
+import factory.management.system.project.service.LineService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * LineStatistics
@@ -16,11 +22,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/server/lineStatistics")
 public class LineStatisticsController {
+    
+    @Autowired
+    private LineService lineService;
 
     @GetMapping("/index")
     public String getLineStatistics(Model model) {
+        // 把信息添加到model中
+        addModel(model);
+        return "index";
+    }
+
+    @PostMapping("/query")
+    public String query(Integer lineId, Model model) {
+        // 获取线路信息
+        Line line = lineService.getLine(lineId);
+        model.addAttribute("line", line);
+        // 把信息添加到model中
+        addModel(model);
+        return "index";
+    }
+
+    /**
+     * 把信息添加到model中
+     */
+    private void addModel(Model model) {
+        // 获取线路列表
+        List<Line> lines = lineService.lineList();
+        model.addAttribute("lineList", lines);
         // 设置分页跳转链接
         model.addAttribute("url", "/server/lineStatistics");
-        return "index";
     }
 }
--- src/main/java/factory/management/system/project/controller/StationStatisticsController.java
+++ src/main/java/factory/management/system/project/controller/StationStatisticsController.java
@@ -1,9 +1,15 @@
 package factory.management.system.project.controller;
 
+import factory.management.system.project.entity.Station;
+import factory.management.system.project.service.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * StationStatisticsController
@@ -17,10 +23,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/server/stationStatistics")
 public class StationStatisticsController {
 
+    @Autowired
+    private StationService stationService;
+
     @GetMapping("/index")
     public String getStationStatistics(Model model) {
+        // 把信息添加到model中
+        addModel(model);
+        return "index";
+    }
+
+    @PostMapping("/query")
+    public String query(Integer stationId, Model model) {
+        // 获取站点信息
+        Station station = stationService.getStation(stationId);
+        model.addAttribute("station", station);
+        // 把信息添加到model中
+        addModel(model);
+        return "index";
+    }
+
+    /**
+     * 把信息添加到model中
+     */
+    private void addModel(Model model) {
+        // 获取站点列表
+        List<Station> stations = stationService.stationList();
+        model.addAttribute("stationList", stations);
         // 设置分页跳转链接
         model.addAttribute("url", "/server/stationStatistics");
-        return "index";
     }
 }
